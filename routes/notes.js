d3706047d94f7a439baacc443f5ac7a4d652dc7b
Jesus Cifuentes <22laloc@gmail.com>
2021-12-13 19:28:23 -0600

    added post line to notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -6,3 +6,27 @@ const { readFromFile, readAndAppend, writeTofile } = require("../helpers/fsUtils
 notes.get("/", (req, res) => {
     readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
   });
+
+
+  notes.post("/", (req,res) => {
+    console.log(req.body);
+    
+    const{ title, text } = req.body;
+
+    if(req.body) {
+        const newNote = {
+            title,
+            text,
+            id: uuidv4(),
+            
+        };
+
+        readAndAppend(newNote, "./db/db.json");
+        res.json("Note has been added")
+    } else {
+        res.error("Error adding note.")
+    }
+
+  });
+
+  module.exports = notes;
