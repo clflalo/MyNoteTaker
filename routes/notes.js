b6baaf3985522bd25739022de1e2c65aadaa12cb
Jesus Cifuentes <22laloc@gmail.com>
2021-12-13 21:01:18 -0600

    added id

--- routes/notes.js
+++ routes/notes.js
@@ -3,14 +3,15 @@ const { v4: uuidv4 } = require ("uuid");
 const { readFromFile, readAndAppend, writeTofile } = require("../helpers/fsUtils")
 
 notes.get("/", (req, res) => {
-    readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+    readFromFile("./db/db.json")
+    .then((data) => res.json(JSON.parse(data)));
   });
 
 
   notes.post("/", (req,res) => {
     console.log(req.body);
     
-    const{ title, text } = req.body;
+    const{title, text, id } = req.body;
 
     if(req.body) {
         const newNote = {
