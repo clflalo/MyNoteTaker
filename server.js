5ebceae2ef692cd92aa247de3e6dfac92b12c6c3
Jesus Cifuentes <22laloc@gmail.com>
2021-12-13 19:48:32 -0600

    fixed notes route

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
 app.use(express.static('public'));
@@ -20,7 +20,7 @@ app.get('/', (req, res) =>
 );
 
 //GET Route for notes page
-app.get('/', (req, res) =>
+app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
